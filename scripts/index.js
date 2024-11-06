e2e1b14e27255191be35c8506e09ef3771767d8b
gollamaheswari <gollamaheswari6@gmail.com>
2024-11-06 05:56:42 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 
 const config = {
   birthdate: 'November 13, 2024',
-  name: 'Prasanna (Miss India)'
+  name: 'Prasanna'
 };
 
 function hideEverything() {
